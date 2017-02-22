dd545ee548896b80fbe10cdbc56730db294563c9
Vikram Manja <vmanja@purdue.edu>
2017-02-21 20:23:11 -0500

    Update rsa_manja.py

--- RSA/rsa_manja.py
+++ RSA/rsa_manja.py
@@ -3,8 +3,86 @@
 #####################################
 
 from BitVector import *
-from PrimeGenerator import PrimeGenerator as PG
 import sys
+import random
+
+import random
+
+############################  class PrimeGenerator   by Avi Kak ##############################
+class PrimeGenerator( object ):                                              #(A1)
+
+    def __init__( self, **kwargs ):                                          #(A2)
+        bits = debug = None                                                  #(A3)
+        if 'bits' in kwargs  :     bits = kwargs.pop('bits')                 #(A4)
+        if 'debug' in kwargs :     debug = kwargs.pop('debug')               #(A5)
+        self.bits            =     bits                                      #(A6)
+        self.debug           =     debug                                     #(A7)
+        self._largest        =     (1 << bits) - 1                           #(A8)
+
+    def set_initial_candidate(self):                                         #(B1)
+        candidate = random.getrandbits( self.bits )                          #(B2)
+        if candidate & 1 == 0: candidate += 1                                #(B3)
+        candidate |= (1 << self.bits-1)                                      #(B4)
+        candidate |= (2 << self.bits-3)                                      #(B5)
+        self.candidate = candidate                                           #(B6)
+
+    def set_probes(self):                                                    #(C1)
+        self.probes = [2,3,5,7,11,13,17]                                     #(C2)
+
+    # This is the same primality testing function as shown earlier
+    # in Section 11.5.6 of Lecture 11:
+    def test_candidate_for_prime(self):                                      #(D1)
+        'returns the probability if candidate is prime with high probability'
+        p = self.candidate                                                   #(D2)
+        if p == 1: return 0                                                  #(D3)
+        if p in self.probes:                                                 #(D4)
+            self.probability_of_prime = 1                                    #(D5)
+            return 1                                                         #(D6)
+        if any([p % a == 0 for a in self.probes]): return 0                  #(D7)
+        k, q = 0, self.candidate-1                                           #(D8)
+        while not q&1:                                                       #(D9)
+            q >>= 1                                                          #(D10)
+            k += 1                                                           #(D11)
+        if self.debug: print("q = %d  k = %d" % (q,k))                       #(D12)
+        for a in self.probes:                                                #(D13)
+            a_raised_to_q = pow(a, q, p)                                     #(D14)
+            if a_raised_to_q == 1 or a_raised_to_q == p-1: continue          #(D15)
+            a_raised_to_jq = a_raised_to_q                                   #(D16)
+            primeflag = 0                                                    #(D17)
+            for j in range(k-1):                                             #(D18)
+                a_raised_to_jq = pow(a_raised_to_jq, 2, p)                   #(D19)
+                if a_raised_to_jq == p-1:                                    #(D20)
+                    primeflag = 1                                            #(D21)
+                    break                                                    #(D22)
+            if not primeflag: return 0                                       #(D23)
+        self.probability_of_prime = 1 - 1.0/(4 ** len(self.probes))          #(D24)
+        return self.probability_of_prime                                     #(D25)
+
+    def findPrime(self):                                                     #(E1)
+        self.set_initial_candidate()                                         #(E2)
+        if self.debug:  print("    candidate is: %d" % self.candidate)       #(E3)
+        self.set_probes()                                                    #(E4)
+        if self.debug:  print("    The probes are: %s" % str(self.probes))   #(E5)
+        max_reached = 0                                                      #(E6)
+        while 1:                                                             #(E7)
+            if self.test_candidate_for_prime():                              #(E8)
+                if self.debug:                                               #(E9)
+                    print("Prime number: %d with probability %f\n" %
+                          (self.candidate, self.probability_of_prime) )      #(E10)
+                break                                                        #(E11)
+            else:                                                            #(E12)
+                if max_reached:                                              #(E13)
+                    self.candidate -= 2                                      #(E14)
+                elif self.candidate >= self._largest - 2:                    #(E15)
+                    max_reached = 1                                          #(E16)
+                    self.candidate -= 2                                      #(E17)
+                else:                                                        #(E18)
+                    self.candidate += 2                                      #(E19)
+                if self.debug:                                               #(E20)
+                    print("    candidate is: %d" % self.candidate)           #(E21)
+        return self.candidate                                                #(E22)
+
+##############################################################################
 
 e = 65537
 
@@ -27,14 +105,14 @@ def coprime(a,b):
     else: return False
 
 def get_two_primes():
-    generator = PG(bits=128)
+    generator = PrimeGenerator(bits=128)
     p = generator.findPrime()
     q = generator.findPrime()
     # check not equal
     if (p==q): return get_two_primes()
     # check coprime
-    if not coprime(p,e): return get_two_primes()
-    if not coprime(q,e): return get_two_primes()
+    if not coprime(p-1,e): return get_two_primes()
+    if not coprime(q-1,e): return get_two_primes()
     return p,q
 
 def gme(a,b,n):
